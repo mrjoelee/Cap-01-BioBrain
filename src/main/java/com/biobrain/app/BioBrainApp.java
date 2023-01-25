9a326df52d3d156f4633b5d859c3e251648a8682
John Ramos <ramos.johnmba@gmail.com>
2023-01-25 15:40:00 -0800

    created method asking player if they want to play Bio Brain, AB#405

--- src/main/java/com/biobrain/app/BioBrainApp.java
+++ src/main/java/com/biobrain/app/BioBrainApp.java
@@ -3,17 +3,41 @@ package com.biobrain.app;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class BioBrainApp {
 
+    Scanner input = new Scanner(System.in);
     public void execute() {
+
         welcome();
+        askIfUserWantToPlay();
     }
 
     private void welcome() {
         printFile("src/main/images/welcomeRobot.txt");
     }
 
+    private void askIfUserWantToPlay() {
+
+        String answer;
+
+        while(true) {
+            System.out.println("Do you want to play Bio Brain? (y/n)");
+            answer = input.nextLine();
+
+            if(answer.equalsIgnoreCase("y")) {
+                System.out.println("Let's play!");
+                // game would start here
+                break;
+            } else if(answer.equalsIgnoreCase("n")) {
+                System.out.println("You are missing out!! Bye!!");
+                System.exit(0);
+            } else {
+                System.out.println("Invalid answer. Please type 'y' or 'n'.");
+            }
+        }
+    }
     private void printFile(String fileName) {
         try{
             System.out.println(Files.readString(Path.of(fileName)));
