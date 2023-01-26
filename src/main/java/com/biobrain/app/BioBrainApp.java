94d6a04eabd6d5dc4f48f80226bdc0f625d2f90a
John Ramos <ramos.johnmba@gmail.com>
2023-01-26 14:46:00 -0800

    fixed quit method, AB#406

--- src/main/java/com/biobrain/app/BioBrainApp.java
+++ src/main/java/com/biobrain/app/BioBrainApp.java
@@ -2,15 +2,26 @@ package com.biobrain.app;
 
 import com.apps.util.Console;
 import com.apps.util.Prompter;
+import com.biobrain.Location;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class BioBrainApp {
 
     private final Prompter prompter = new Prompter(new Scanner(System.in));
+    private Location currentLocation;
+    private List<Location> locations;
     private boolean gameOver = false;
 
     public void execute() {
@@ -19,6 +30,15 @@ public class BioBrainApp {
         welcome();
         Console.pause(1500);
         askIfUserWantToPlay();
+
+//        Gson gson = new Gson();
+//        Type locationList = new TypeToken<List<Location>>() {
+//        }.getType();
+//        try (BufferedReader reader = new BufferedReader(new FileReader("jsonFiles/locations.json"))) {
+//            List<Location> locations = gson.fromJson(reader, locationList);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public void intro() {
@@ -49,6 +69,26 @@ public class BioBrainApp {
 
     private void game() {
 
+        Gson gson = new Gson();
+        Type locationList = new TypeToken<List<Location>>() {
+        }.getType();
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/jsonFiles/locations.json"))) {
+
+            locations = gson.fromJson(reader, locationList);
+
+            if(locations != null && !locations.isEmpty()){
+
+                currentLocation = locations.get(0);
+
+                System.out.printf("You are currently in " + currentLocation.getName());
+            } else {
+                System.out.println("Error in getting the location");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         Thread inputThread = new Thread(() -> {
             Scanner scanner = new Scanner(System.in);
             while (!gameOver) {
@@ -64,6 +104,7 @@ public class BioBrainApp {
 
         if (!gameOver) {
             printFile("src/main/images/mapBioBrain.txt");
+
         }
     }
 
@@ -74,5 +115,6 @@ public class BioBrainApp {
             e.printStackTrace();
         }
     }
+
 
 }
