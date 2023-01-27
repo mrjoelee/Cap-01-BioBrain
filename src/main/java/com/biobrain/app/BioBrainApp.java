15a367a86c5dac4cd7e8843ec2ea3c2b24aa2e88
Sarina Lyons <sarinalyons@gmail.com>
2023-01-27 12:32:58 -0500

    Ticket409

--- src/main/java/com/biobrain/app/BioBrainApp.java
+++ src/main/java/com/biobrain/app/BioBrainApp.java
@@ -1,58 +1,32 @@
 package com.biobrain.app;
 
-import com.apps.util.Console;
-import com.apps.util.Prompter;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Scanner;
 
 public class BioBrainApp {
 
-    private final Prompter prompter = new Prompter(new Scanner(System.in));
-    private boolean gameOver = false;
-
     public void execute() {
-
+        intro();
         welcome();
-        Console.pause(1500);
-        askIfUserWantToPlay();
+    }
+
+    public void intro() {
+        printFile("src/main/intro/intro.txt");
     }
 
     private void welcome() {
-        String splashScreen = "src/main/images/welcomeRobot.txt";
-        printFile(splashScreen);
-    }
-
-    private void askIfUserWantToPlay() {
-
-        String dontWantToPlayBanner = "src/main/images/dontWantToPlayBanner.txt";
-        System.out.println("\nWould you like to play Bio Brain? [Y]es or [N]o ");
-        String input = prompter.prompt("Enter response: ", "[YyNn]", "\nInvalid input... Please enter [Y]es or [N]o \n");
-
-        if (input.equalsIgnoreCase("y")) {
-
-            System.out.println("Let's play!");
-            // this is where we start the game
-            game();
-        } else {
-            printFile(dontWantToPlayBanner);
-        }
-    }
-
-    private void game() {
-
-        if (!gameOver){
-            printFile("src/main/images/mapBioBrain.txt");
-        }
+        printFile("src/main/images/welcomeRobot.txt");
     }
 
     private void printFile(String fileName) {
-        try {
+        try{
             System.out.println(Files.readString(Path.of(fileName)));
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
 }
