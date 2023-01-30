a02dc989567ef45d3860c2f018462bd65ad85a6f
John Ramos <ramos.johnmba@gmail.com>
2023-01-30 13:32:57 -0800

    fixed look at item, AB#412

--- src/main/java/com/biobrain/app/BioBrainApp.java
+++ src/main/java/com/biobrain/app/BioBrainApp.java
@@ -13,6 +13,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.stream.Stream;
@@ -22,8 +23,8 @@ public class BioBrainApp {
     private final Prompter prompter = new Prompter(new Scanner(System.in));
     private Location currentLocation;
     private List<Location> locations;
+    private List<String> itemsInRoom;
     private boolean gameOver = false;
-
 
     public void execute() {
         intro();
@@ -62,11 +63,16 @@ public class BioBrainApp {
 
     private void game() {
         locationsJsonParsed();
+//        startGame();
 
         if (!gameOver) {
 //            printFile("images/mapBioBrain.txt");
-            askPlayerAction();
+          askPlayerAction();
         }
+
+//        while (!gameOver) {
+//            askPlayerAction();
+//        }
     }
 
     private void locationsJsonParsed() {
@@ -83,7 +89,7 @@ public class BioBrainApp {
 
                 currentLocation = locations.get(0);
 
-                List<String> itemsInRoom = currentLocation.getItems();
+                itemsInRoom = currentLocation.getItems();
 
                 System.out.printf("\nYou are currently in %s \n", currentLocation.getName());
                 System.out.println("\nYou see the following items: ");
@@ -102,24 +108,52 @@ public class BioBrainApp {
         }
     }
 
+
+
     private void askPlayerAction() {
         System.out.println("\nWhat would you like to do? Look at items or Move to a different location");
         System.out.println("\nType Look to check item or Type Go to the direction you want to move to.");
-        String input = prompter.prompt("\nEnter response: ", "[LlMm]", "\nInvalid input... Please enter [L]ook or [M]ove \n");
-        if(input.equalsIgnoreCase("l")) {
+        String input = prompter.prompt("\nEnter response: ", "[LlMm]", "\nInvalid input... Please type Look or Move \n");
+        if (input.equalsIgnoreCase("l")) {
             System.out.println("\nWhich item would you like to look at?");
             String itemToLookAt = prompter.prompt("Enter item name: ");
-            if(currentLocation.getItems().contains(itemToLookAt)) {
-                System.out.printf("\nYou are looking at %s", itemToLookAt);
-//                String itemDescription = Item.getDescriptions(itemToLookAt);
-//                System.out.println("\nItem description: " + itemDescription);
+            if (currentLocation.getItems().contains(itemToLookAt)) {
+                String itemDescription = Item.getDescriptions(itemToLookAt);
+                int damageValue = Item.getDamageValue(itemToLookAt);
+                System.out.printf("\nItem description:  %s it has a damage value of %s", itemDescription, damageValue);
 
             } else {
                 System.out.println("\nItem not found");
             }
         } else {
-            System.out.println("You are moving to a different location");
+            System.out.println("\nWhich direction would you like to move to?");
+            String direction = prompter.prompt("Enter direction: ", "[EeSs]", "\nInvalid input... Please type East or South \n");
+            movePlayer(direction);
         }
+
+    }
+
+    private void movePlayer(String direction) {
+            String nextLocation = currentLocation.getDirections().get(direction.toLowerCase());
+            if(nextLocation != null){
+                for(Location location : locations){
+                    if(location.getName().equalsIgnoreCase(nextLocation)) {
+                        currentLocation = location;
+                        break;
+                    }
+                }
+                itemsInRoom = currentLocation.getItems();
+                System.out.printf("\nYou are currently in %s \n", currentLocation.getName());
+                System.out.println("\nYou see the following items: ");
+                for (String item : itemsInRoom) {
+                    System.out.print("\n " + item);
+                }
+                System.out.printf("\n\nYou can choose to go East to %s ", currentLocation.getDirections().get("east"));
+                System.out.printf("\nOr you can go South to %s", currentLocation.getDirections().get("south"));
+            }
+            else {
+                System.out.println("\nYou can't go that way");
+            }
     }
 
     private void printFile(String fileName) {
