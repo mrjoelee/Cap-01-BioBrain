a3e13f49772a20b8fb5d12c47f035a57c8b12e85
wrijlarr <wrijlarr@amazon.com>
2023-02-03 12:09:40 -0500

    Load game class and Save game class AB#435 AB#436

---
+++ src/main/java/com/biobrain/SaveGame.java
@@ -0,0 +1,24 @@
+package com.biobrain;
+
+import com.biobrain.app.BioBrainApp;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class SaveGame implements java.io.Serializable {
+    private static Object BioBrainApp;
+
+    private static void saveGame() {
+        try {
+            FileOutputStream fos = new FileOutputStream("Bb.sav");
+            ObjectOutputStream os = new ObjectOutputStream(fos);
+            os.writeObject(BioBrainApp);// game info
+            os.flush();
+            os.close();
+            System.out.println("Game saved\n");
+        } catch (IOException e){
+            System.out.println("Error! Cant save data.\n" + e.getMessage());
+        }
+    }
+}
