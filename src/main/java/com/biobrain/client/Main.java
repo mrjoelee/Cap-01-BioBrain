ca991099da105d7112e9a2769667727df7a0b4f0
Michael Greene <mgreene11111@yahoo.com>
2023-02-07 14:28:10 -0800

    Committing changes for merge

--- src/main/java/com/biobrain/client/Main.java
+++ src/main/java/com/biobrain/client/Main.java
@@ -2,12 +2,16 @@ package com.biobrain.client;
 
 import com.biobrain.app.BioBrainApp;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         BioBrainApp app = new BioBrainApp();
         app.execute();
+
+        String help = new StringBuffer().append("READ ME").toString();
+        JOptionPane.showMessageDialog(null, help);
     }
 }
 
